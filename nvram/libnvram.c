5e0dfed0469a5fed603e8288bb3f72dd00d21c4d
Bharat Dev Burman <bharat.singh7924@gmail.com>
2020-07-12 14:03:56 +0000

    libnvram.c: comment out undefined include

--- nvram/libnvram.c
+++ nvram/libnvram.c
@@ -66,7 +66,7 @@
 //#include "CFG_file_lid.h"
 #include <dirent.h>
 #include <sys/mount.h>
-#include <fs_mgr.h>
+//#include <fs_mgr.h>
 #include <fstab.h>
 #include <cutils/properties.h>
 #include "libnvram_log.h"
