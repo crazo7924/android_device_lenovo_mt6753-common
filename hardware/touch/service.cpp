44c622add448ea5d64ff1b52d468b6cd0c2a0906
Bharat Dev Burman <bharat.singh7924@gmail.com>
2020-08-08 15:00:29 +0000

    touch: shift to vendor

--- hardware/touch/service.cpp
+++ hardware/touch/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "lineage.touch@1.0-service.lenovo"
+#define LOG_TAG "vendor.lineage.touch@1.0-service.lenovo"
 
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
